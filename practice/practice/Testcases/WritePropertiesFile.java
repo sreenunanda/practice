ae2f62ec1e4aaf2b1addfbe1fc711b90b59c377a
Sreenu <sr.nanda8@gmail.com>
2018-02-20 11:23:46 +0530

    ++updated WritePropertiesFile

--- practice/practice/Testcases/WritePropertiesFile.java
+++ practice/practice/Testcases/WritePropertiesFile.java
@@ -4,16 +4,24 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 public class WritePropertiesFile {
 	public static void main(String[] args) {
 		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("ddyyyymss");
+			String getdate  = dateFormat.format(new Date());
 			Properties properties = new Properties();
-			properties.setProperty("Date", "12Aug2018");
-			properties.setProperty("email", "23@gmail.com");
-			properties.setProperty("phonenumber", "1234567");
-
+			properties.setProperty("Date", getdate);
+			properties.setProperty("email", getdate+"@girmiti.com");
+			properties.setProperty("phonenumber", getdate);
+			properties.setProperty("modelname", getdate);
+			
+			//trail
+			System.out.println(properties.getProperty("Date"));
+			
 			File file = new File("test2.properties");
 			FileOutputStream fileOut = new FileOutputStream(file);
 			properties.store(fileOut, "appending values");
